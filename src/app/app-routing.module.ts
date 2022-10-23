e2a0b436eae97c93255b1067adb5b57fbca17de8
Florian Holler <holler.florian.87@gmail.com>
2022-10-23 12:39:02 +0200

    Added the home website to the components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'home',
+  component: HomeComponent
+},
+// When no path is entered, it will redirect to the home url
+{ 
+  path: '', redirectTo: 'home', pathMatch: 'full'
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
