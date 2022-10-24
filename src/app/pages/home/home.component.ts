86bba22b2aba17df221add4617b4ede35c441017
Florian Holler <holler.florian.87@gmail.com>
2022-10-24 14:06:19 +0200

    Added mockup-products

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+const ROWS_HEIGHT: { [id:number]: number } = { 1: 400, 3: 335, 4: 350 }
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent implements OnInit {
   cols = 3;
+  rowHeight = ROWS_HEIGHT[this.cols];
+  category: string | undefined;
 
   constructor() { }
 
@@ -14,5 +18,11 @@ export class HomeComponent implements OnInit {
 
   onColumnsCountChange(colsNum: number): void {
     this.cols = colsNum;
+    // Updating the row height dynamically
+    this.rowHeight = ROWS_HEIGHT[this.cols];
+  }
+
+  onShowCategory(newCategory: string): void {
+    this.category = newCategory;
   }
 }
