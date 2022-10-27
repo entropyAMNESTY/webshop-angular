3660942c107e1487068169fd584958713c51246f
Florian Holler <holler.florian.87@gmail.com>
2022-10-27 15:22:46 +0200

    Added items from the API to the Webshop

--- src/app/pages/home/components/product-box/product-box.component.ts
+++ src/app/pages/home/components/product-box/product-box.component.ts
@@ -8,14 +8,7 @@ import { Product } from 'src/app/models/product.model';
 export class ProductBoxComponent implements OnInit {
   // Setting up the product-views
   @Input() fullWidthMode = false;
-  product: Product | undefined = {
-    id: 1,
-    title: 'Sneakers',
-    price: 150,
-    category: 'Shoes',
-    description: 'Description',
-    image: 'https://via.placeholder.com/150',
-  };
+  @Input() product: Product | undefined;
 
   // Communicating with the child / parent component
   @Output() addToCart = new EventEmitter()
