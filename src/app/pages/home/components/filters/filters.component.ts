8d4e82c6c5b3ace509d1e43db70fec053e743fed
entropyAMNESTY <entropy.random.squared@gmail.com>
2022-10-29 10:53:10 +0200

    Integrated APi sho-items

--- src/app/pages/home/components/filters/filters.component.ts
+++ src/app/pages/home/components/filters/filters.component.ts
@@ -1,4 +1,6 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { StoreService } from 'src/app/services/store.service';
 
 @Component({
   selector: 'app-filters',
@@ -6,16 +8,28 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 
 })
-export class FiltersComponent implements OnInit {
-  @Output() showCategory = new EventEmitter<string>();  
-  categories = ['shoes', 'sports'];
-  constructor() { }
+export class FiltersComponent implements OnInit, OnDestroy {
+  @Output() showCategory = new EventEmitter<string>(); 
+  categoriesSubscription: Subscription | undefined; 
+  categories: Array<string> | undefined
+
+  constructor(private storeService: StoreService) { }
 
   ngOnInit(): void {
-  }
+    this.categoriesSubscription = this.storeService.getAllCategories()
+      .subscribe((response) => {
+        this.categories = response;
+      })
+    }
 
   onShowCategory(category: string): void {
     this.showCategory.emit(category);
+  }
+
+  ngOnDestroy(): void {
+    if(this.categoriesSubscription) {
+      this.categoriesSubscription.unsubscribe();
+    }
   }
 
 }
